35880cf75c55e970d5de2617197d0606f6b3bad3
Piotrek Dubiel <piotr.dubiel@polidea.com>
2016-05-04 09:13:25 +0200

    Mock image sources with mockery

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,6 +1,7 @@
 import React, {
   StyleSheet,
   PropTypes,
+  Image,
   View,
   Text,
 } from 'react-native';
@@ -9,6 +10,7 @@ export default class MyComponent extends React.Component {
   render() {
     return (
       <View>
+        <Image source={require('./react.png')}/>
         <Text>I wonder if there will be any problems...</Text>
       </View>
     );
--- test/MyComponent.js
+++ test/MyComponent.js
@@ -1,12 +1,25 @@
-import React, { View, Text, StyleSheet } from 'react-native';
+import React, { View, Image, Text, StyleSheet } from 'react-native';
 import { shallow } from 'enzyme';
 import MyComponent from '../src/MyComponent';
 import { expect } from 'chai';
+import mockery from 'mockery';
+
+const IMAGE_SUBSTITUTE = {uri: 'placeholder.png'};
 
 describe('<MyComponent />', () => {
+  before(() => {
+    mockery.enable();
+    mockery.registerMock('./react.png', IMAGE_SUBSTITUTE);
+  });
+
+  after(() => {
+    mockery.disable();
+  });
+
   it('should render stuff', () => {
     const wrapper = shallow(<MyComponent />);
     expect(wrapper.length).to.equal(1);
     expect(wrapper.contains(<Text>I wonder if there will be any problems...</Text>)).to.equal(true);
+    expect(wrapper.contains(<Image source={IMAGE_SUBSTITUTE}/>)).to.equal(true);
   });
 });
